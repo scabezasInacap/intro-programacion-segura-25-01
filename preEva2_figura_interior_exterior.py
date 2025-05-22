4e6a8c6936cb2dcac365cb6423fda8cdb6efcda6
scabezasInacap <sebastian.cabezas07@inacapmail.cl>
2025-05-22 14:50:44 -0400

    Create preEva2_figura_interior_exterior.py

---
+++ preEva2_figura_interior_exterior.py
@@ -0,0 +1,18 @@
+# $ $ $ $ $ $ $
+# $ $ $ * $ $ $ 
+# $ $ * * * $ $
+# $ * * * * * $
+# $ $ * * * $ $
+# $ $ $ * $ $ $
+# $ $ $ $ $ $ $
+
+
+# Considera la condición de borde para ingresar un numero y representar la figura
+# La función debe recibir 3 valores como parámetros: _caracterInterior, _caracterExterior, _numero
+# Debe validar que el numero ingresado sea solo un numero impar.
+# Debe validar que caracter interior y caracter exterior solo tenga un caracter.
+
+# Evaluacion 2:
+# Sentencias de control: If, Else, Elif
+# Control de ciclos: While, For
+# Funciones
