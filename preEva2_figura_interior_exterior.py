3efadd727e7d7fb5b66057b605d6646e66f80cb6
scabezasInacap <sebastian.cabezas07@inacapmail.cl>
2025-05-23 16:26:08 -0400

    Update preEva2_figura_interior_exterior.py

--- preEva2_figura_interior_exterior.py
+++ preEva2_figura_interior_exterior.py
@@ -16,3 +16,22 @@
 # Sentencias de control: If, Else, Elif
 # Control de ciclos: While, For
 # Funciones
+
+def imprimeFigura(_ci, _ce, _n):
+    #print(f"ci: {_ci} ce: {_ce} n: {_n}")
+    iterador = 0
+    while iterador < _n:
+        mitad = int(_n/2)
+        if (iterador == 0 or iterador == (_n -1)):
+            print(f"{_ce * _n}")
+        else:
+            if (iterador <= mitad):
+                cantidadInterior = (iterador + (1 * iterador) - 1)
+            else:
+                cantidadInterior = cantidadInterior - 2
+            cantidadMitadExterior = int((_n - cantidadInterior)/2)
+            print(f"{_ce * cantidadMitadExterior}{_ci * cantidadInterior}{_ce * cantidadMitadExterior}")
+        iterador += 1
+
+#APP
+imprimeFigura(' ','#',21)
